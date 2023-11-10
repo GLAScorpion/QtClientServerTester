b3bc1cde673f3a2044cc6f62186e5e3e1188285b
GLAScorpion <53878421+GLAScorpion@users.noreply.github.com>
2023-11-10 11:57:02 +0100

    Update main.cpp

--- QtServerTester/main.cpp
+++ QtServerTester/main.cpp
@@ -13,3 +13,28 @@ int main(int argc, char *argv[])
     w.show();
     return a.exec();
 }
+QCoreApplication* createApplication(int &argc, char *argv[])
+{
+    for (int i = 1; i < argc; ++i) {
+        if (!qstrcmp(argv[i], "-no-gui"))
+            return new QCoreApplication(argc, argv);
+    }
+    return new QApplication(argc, argv);
+}
+
+int main(int argc, char* argv[])
+{
+    QScopedPointer<QCoreApplication> app(createApplication(argc, argv));
+
+    if (qobject_cast<QApplication *>(app.data())) {
+        // start GUI version...
+        MyServer server;
+        MainWindow w;
+        w.ServerBind(&server);
+        w.show();
+    } else {
+        // start non-GUI version...
+    }
+
+    return app->exec();
+}
